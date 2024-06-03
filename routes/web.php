aa6b5a7b2cbd22eab9dab739c92b6dcf73897b1d
XzRnz <arunaarya19@gmail.com>
2024-06-04 00:13:58 +0700

    membuat Layout

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
-
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/',[AppController::class,'index'])->name('app.index');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
