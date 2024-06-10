88c4a13b64841f98a383a59b9f691ee67f1663a1
XzRnz <arunaarya19@gmail.com>
2024-06-10 23:12:22 +0700

    mengcustom page

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -527,7 +527,7 @@
     </div>
     @endforeach
 </div>
-{{$products->links()}}
+{{$products->links("pagination.default")}}
 
                         <div>
                             <div class="product-box">
